53f843617c026240a8b1c889a5e024cb253fb118
KigoJomo <kigojomo@gmail.com>
2024-07-09 02:26:35 +0300

    Shipment Tracking Map

--- src/components/MapComponents/MapboxComponent.js
+++ src/components/MapComponents/MapboxComponent.js
@@ -6,8 +6,6 @@ import "mapbox-gl/dist/mapbox-gl.css";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-// shipmentDetails.currentLocation
-
 const MapBoxComponent = ({ shipmentDetails }) => {
   const mapContainerRef = useRef(null);
   const map = useRef(null);
@@ -16,10 +14,11 @@ const MapBoxComponent = ({ shipmentDetails }) => {
     if (!map.current) {
       map.current = new mapboxgl.Map({
         container: mapContainerRef.current,
-        style: "mapbox://styles/mapbox/streets-v11",
-        center: [36.9609, -0.3978],
-        zoom: 15,
-        pitch: 50,
+        // style: "mapbox://styles/mapbox/streets-v11",
+        style: "mapbox://styles/kigojomo/clvl9b8gn009h01pc7gou2uwf",
+        center: [36, 2],
+        zoom: 1,
+        pitch: 0,
       });
     }
   }, []);
@@ -28,20 +27,127 @@ const MapBoxComponent = ({ shipmentDetails }) => {
     if (shipmentDetails && map.current) {
       const { currentLocation, destination, manufacturer } = shipmentDetails;
 
-      // map.current.setCenter(currentLocation);
       map.current.easeTo({
         center: currentLocation,
+        zoom: 15,
+        pitch: 50,
         duration: 3000,
       });
-      new mapboxgl.Marker().setLngLat(currentLocation).addTo(map.current);
-      new mapboxgl.Marker().setLngLat(destination).addTo(map.current);
-      new mapboxgl.Marker().setLngLat(manufacturer).addTo(map.current);
+
+      const manufacturerToCurrent = [manufacturer, currentLocation];
+      const currentToDestination = [currentLocation, destination];
+
+      // Add markers with popups
+      new mapboxgl.Marker()
+        .setLngLat(currentLocation)
+        .setPopup(
+          new mapboxgl.Popup({ offset: 25 })
+            .setHTML(`<h4>Current Location</h4>`)
+            .addTo(map.current)
+        )
+        .addTo(map.current)
+        .togglePopup();
+
+      new mapboxgl.Marker()
+        .setLngLat(destination)
+        .setPopup(
+          new mapboxgl.Popup({ offset: 25 })
+            .setHTML(`<h4>Destination</h4>`)
+            .addTo(map.current)
+        )
+        .addTo(map.current)
+        .togglePopup();
+
+      new mapboxgl.Marker()
+        .setLngLat(manufacturer)
+        .setPopup(
+          new mapboxgl.Popup({ offset: 25 })
+            .setHTML(`<h4>Manufacturer</h4>`)
+            .addTo(map.current)
+        )
+        .addTo(map.current)
+        .togglePopup();
+
+      if (map.current.getSource("route-manufacturer-to-current")) {
+        map.current.getSource("route-manufacturer-to-current").setData({
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "LineString",
+            coordinates: manufacturerToCurrent,
+          },
+        });
+      } else {
+        map.current.addSource("route-manufacturer-to-current", {
+          type: "geojson",
+          data: {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "LineString",
+              coordinates: manufacturerToCurrent,
+            },
+          },
+        });
+
+        map.current.addLayer({
+          id: "route-manufacturer-to-current",
+          type: "line",
+          source: "route-manufacturer-to-current",
+          layout: {
+            "line-join": "round",
+            "line-cap": "round",
+          },
+          paint: {
+            "line-color": "#0050fc",
+            "line-width": 8,
+          },
+        });
+      }
+
+      if (map.current.getSource("route-current-to-destination")) {
+        map.current.getSource("route-current-to-destination").setData({
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "LineString",
+            coordinates: currentToDestination,
+          },
+        });
+      } else {
+        map.current.addSource("route-current-to-destination", {
+          type: "geojson",
+          data: {
+            type: "Feature",
+            properties: {},
+            geometry: {
+              type: "LineString",
+              coordinates: currentToDestination,
+            },
+          },
+        });
+
+        map.current.addLayer({
+          id: "route-current-to-destination",
+          type: "line",
+          source: "route-current-to-destination",
+          layout: {
+            "line-join": "round",
+            "line-cap": "round",
+          },
+          paint: {
+            "line-color": "#608dee",
+            "line-width": 8,
+            "line-dasharray": [3, 3],
+          },
+        });
+      }
     }
   }, [shipmentDetails]);
 
   return (
     <div
-      className="map-container h-full border border-l-0 border-slate-200 rounded-lg rounded-l-none"
+      className="map-container h-full border border-l-0 border-slate-200 rounded-lg rounded-l-none bg-stone-900"
       ref={mapContainerRef}
     ></div>
   );
