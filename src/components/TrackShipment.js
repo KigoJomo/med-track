a4921c1d5dcf2e5d318a4713aef79c846dece28c
KigoJomo <kigojomo@gmail.com>
2024-07-08 09:15:57 +0300

    TrackShipment Outline

--- src/components/TrackShipment.js
+++ src/components/TrackShipment.js
@@ -2,19 +2,40 @@ import React from "react";
 
 function TrackShipment() {
   return (
-    <div className="h-full p-4 ">
-      <h2 className="text-2xl font-bold">Track Shipment</h2>
-      <div className="p-4 mt-4 bg-white rounded shadow">
-        <p>Shipment ID: SH12345</p>
-        <p>Current Location: Nairobi</p>
-        <p>Temperature: 22°C</p>
-        <p>Humidity: 60%</p>
-        <div id="map" className="mt-4 h-64 bg-gray-300">
-          {" "}
-          {/* Embed a map for real-time tracking */}{" "}
-        </div>
+    <section className="shadow-xl w-full h-full flex items-center gap-8  rounded-xl overflow-hidden">
+      <div className="h-full w-2/5 bg-white px-8 py-6 flex flex-col items-start gap-4">
+
+        <form className="w-full flex items-center gap-2 relative">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            required
+            placeholder="Enter Shipment ID"
+            className="text-gray-800 text-sm rounded-full pl-12 py-2 bg-slate-200 w-full outline-none border-2 border-transparent focus:bg-white focus:outline-none focus:border-indigo-600 transition-all"
+          />
+          <span className="material-symbols-outlined absolute text-xl text-gray-600 left-4">
+            search
+          </span>
+          <button
+            type="submit"
+            className="max-h-full px-2 bg-indigo-600 text-white text-2xl rounded-full hover:bg-indigo-500"
+          >
+            &#x279B;
+          </button>
+        </form>
+
+        <h3 id="enteredId" className="text-base font-semibold text-gray-900 select-none capitalize">
+          SHIP1234
+        </h3>
+
+        {/* timeline */}
+
+        {/* other details */}
       </div>
-    </div>
+
+      <div id="map" className="h-full w-3/5"></div>
+    </section>
   );
 }
 
